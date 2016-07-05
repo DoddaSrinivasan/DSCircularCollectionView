2ea59fb5199384fd7598c042def0b5957517cc95
srinivasandodda <sridodda@thoughtworks.com>
2016-07-05 18:31:12 +0530

    @Srini - Initial commit. Circular view supporting vertical and horizontal scroll

---
+++ Example/DSCircularCollectionViewExample/DSCircularCollectionViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DSCircularCollectionViewExample
+//
+//  Created by Srinivasan Dodda on 04/07/16.
+//  Copyright © 2016 Srinivasan Dodda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/DSCircularCollectionViewExample/DSCircularCollectionViewExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DSCircularCollectionViewExample
+//
+//  Created by Srinivasan Dodda on 04/07/16.
+//  Copyright © 2016 Srinivasan Dodda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
